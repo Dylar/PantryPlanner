b7531422c76e7011997f14d5ecdf74cb2bd1783b
Kolja Bruns <Kolja-bruns@gmx.de>
2023-06-12 11:53:56 +0200

    loading screen

--- app/src/main/kotlin/de/bitb/pantryplaner/ui/intro/SplashFragment.kt
+++ app/src/main/kotlin/de/bitb/pantryplaner/ui/intro/SplashFragment.kt
@@ -1,9 +1,19 @@
 package de.bitb.pantryplaner.ui.intro
 
 import android.os.Bundle
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
 import androidx.fragment.app.viewModels
 import dagger.hilt.android.AndroidEntryPoint
+import de.bitb.pantryplaner.R
 import de.bitb.pantryplaner.ui.base.BaseFragment
 import de.bitb.pantryplaner.ui.base.composable.LoadingIndicator
 
@@ -17,6 +27,20 @@ class SplashFragment : BaseFragment<SplashViewModel>() {
     }
 
     @Composable
-    override fun ScreenContent() = LoadingIndicator()
+    override fun ScreenContent() {
+        Column(
+            modifier = Modifier.fillMaxSize(),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Image(
+                painterResource(R.drawable.ic_launcher),
+                contentDescription = "",
+                contentScale = ContentScale.Crop,
+                modifier = Modifier.width(220.dp).height(220.dp).clip(shape = CircleShape)
+            )
+            LoadingIndicator(modifier = Modifier.height(100.dp).padding(top = 16.dp))
+        }
+    }
 
 }
