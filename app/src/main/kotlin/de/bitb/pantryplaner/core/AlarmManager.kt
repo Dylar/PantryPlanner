410e6af9b384342a846007d8e9422e8d7c416c5b
Kolja Bruns <Kolja-bruns@gmx.de>
2023-10-21 14:30:52 +0200

    fix android 13 crash

--- app/src/main/kotlin/de/bitb/pantryplaner/core/AlarmManager.kt
+++ app/src/main/kotlin/de/bitb/pantryplaner/core/AlarmManager.kt
@@ -5,6 +5,7 @@ import android.app.PendingIntent
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
+import android.os.Build
 import dagger.hilt.android.AndroidEntryPoint
 import de.bitb.pantryplaner.BuildConfig
 import de.bitb.pantryplaner.core.misc.Resource
@@ -23,25 +24,30 @@ object AlertManager {
 
         val calendar: Calendar = Calendar.getInstance()
         calendar.timeInMillis = System.currentTimeMillis()
-        if (isDev) calendar.set(Calendar.MINUTE, 1)
+        if (isDev) calendar.add(Calendar.MINUTE, 1)
         else calendar.set(Calendar.HOUR_OF_DAY, 8)
+
+        val isNewVersion = Build.VERSION.SDK_INT >= Build.VERSION_CODES.S
+        val pendingIntentFlags =
+            if (isNewVersion) PendingIntent.FLAG_UPDATE_CURRENT or PendingIntent.FLAG_IMMUTABLE
+            else PendingIntent.FLAG_UPDATE_CURRENT
 
         val pendingIntent = PendingIntent.getBroadcast(
             context,
             ALARM_REQUEST_CODE,
             Intent(context, AlarmReceiver::class.java),
-            PendingIntent.FLAG_UPDATE_CURRENT
+            pendingIntentFlags
         )
+
+        val alarmManager = context.getSystemService(Context.ALARM_SERVICE) as AlarmManager
 
         val repeatingInterval =
             if (isDev) AlarmManager.INTERVAL_FIFTEEN_MINUTES
             else AlarmManager.INTERVAL_DAY
 
-        val alarmManager = context.getSystemService(Context.ALARM_SERVICE) as AlarmManager
-        alarmManager.setRepeating(
+        alarmManager.setAndAllowWhileIdle(
             AlarmManager.RTC_WAKEUP,
-            calendar.timeInMillis,
-            repeatingInterval,
+            calendar.timeInMillis + repeatingInterval,
             pendingIntent
         )
     }
